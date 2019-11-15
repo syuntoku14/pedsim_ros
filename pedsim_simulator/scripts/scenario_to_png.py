4e052acc6924455c10e211c35de484897e5d4ec8
syuntoku14 <syuntoku14@keio.jp>
2019-11-15 14:42:24 +0900

    Fixing bugs on scenario to png.py

--- pedsim_simulator/scripts/scenario_to_png.py
+++ pedsim_simulator/scripts/scenario_to_png.py
@@ -1,54 +1,54 @@
 #!/usr/bin/env python
-import png, rospkg, numpy
+
+import pdb
+import png
+import cv2
+import numpy as np
+import argparse
+import os
 import xml.etree.ElementTree as xml
 
-filename = "social_contexts.xml"
+parser = argparse.ArgumentParser()
+parser.add_argument('filepath', type=str)
+args = parser.parse_args()
 
-path = ""
-if not "/" in filename:
-    rospack = rospkg.RosPack()
-    path = rospack.get_path("pedsim_simulator") + "/scenarios/"
+if not os.path.isfile(args.filepath):
+    raise ValueError(args.filepath + " doesn't exist")
 
-filepath = path + filename
-print "Reading file " + filepath
+filepath = args.filepath
+print("Reading file: " + filepath)
 
 tree = xml.parse(filepath)
 root = tree.getroot()
 
-xmin = 9999999999
-ymin = 9999999999
-xmax = 0
-ymax = 0
-
 obstacles = []
 for obstacle in root.findall("obstacle"):
-    xlimits = int(obstacle.get("x1")), int(obstacle.get("x2"))
-    ylimits = int(obstacle.get("y1")), int(obstacle.get("y2"))
-    obstacle = ( min(xlimits), min(ylimits), max(xlimits), max(ylimits))
-    xmin = min(xmin, obstacle[0])
-    ymin = min(ymin, obstacle[1])
-    xmax = max(xmax, obstacle[2])
-    ymax = max(ymax, obstacle[3])
+    xlimits = float(obstacle.get("x1")), float(obstacle.get("x2"))
+    ylimits = float(obstacle.get("y1")), float(obstacle.get("y2"))
+    obstacle = (np.floor(min(xlimits)), np.floor(min(ylimits)),
+                np.ceil(max(xlimits)), np.ceil(max(ylimits)))
     obstacles.append(obstacle)
 
-print "Map dimensions: (%d, %d) -- (%d, %d)" % (xmin, ymin, xmax, ymax)
-assert(xmin >= 0)
-assert(ymin >= 0)
+obstacles = np.array(obstacles).astype(np.int)
+print(obstacles)
+xmin = np.min(obstacles[:, 0])
+ymin = np.min(obstacles[:, 1])
+xmax = np.max(obstacles[:, 2])
+ymax = np.max(obstacles[:, 3])
+
+print "Map dimensions: (%f, %f) -- (%f, %f)" % (xmin, ymin, xmax, ymax)
 width = xmax + 1
 height = ymax + 1
 
-grid = numpy.uint8( numpy.zeros( (height, width) ) )
+grid = numpy.uint8(numpy.zeros((height, width)))
 
 for obstacle in obstacles:
     for x in xrange(obstacle[0], obstacle[2] + 1):
         for y in xrange(obstacle[1], obstacle[3] + 1):
             grid[x, y] = 255
 
-outputFilename = filename
-if ".xml" in outputFilename:
-    outputFilename = outputFilename.replace(".xml", ".png")
-else:
-    outputFilename += ".png"
+outputFilename = filepath
+outputFilename = outputFilename.replace(".xml", ".png")
 
 print "Writing output to " + outputFilename
 f = open(outputFilename, 'wb')      # binary mode is important
